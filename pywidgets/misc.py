9408377859dce97fe67eb8115999ac4d0e5206ad
Phil <phil.munzke@icloud.com>
2025-12-21 13:35:14 +0100

    Initial commit 3

--- pywidgets/misc.py
+++ pywidgets/misc.py
@@ -1,10 +1,14 @@
 import requests
 def check_update():
-    response = requests.get("https://github.com/PizzaPost/pywidgets/info")
-    data = response.json()
-    latest_version = data["version"]
-
-    if latest_version == "0.1.0":
-        print("pywidgets is on the latest version")
-    else:
-        print("An update is available. Download it now with 'pip install --upgrade pywidgets'")
+    url = "https://raw.githubusercontent.com/PizzaPost/pywidgets/master/info.json"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        latest_version = data["version"]
+        if latest_version == "0.1.0":
+            print("pywidgets is on the latest version")
+        else:
+            print("An update is available. Download it now with 'pip install --upgrade pywidgets'")
+    except Exception as e:
+        print(f"pywidgets: Failed to check for updates: {e}")
