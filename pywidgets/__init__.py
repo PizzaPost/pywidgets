9e25030de61f979184365aa4a3f85b34c6b68001
Phil <phil.munzke@icloud.com>
2025-12-22 11:38:16 +0100

    first button implementation (no animations yet)

--- pywidgets/__init__.py
+++ pywidgets/__init__.py
@@ -1,1 +1,19 @@
+from .button import Button
 from .misc import check_update, link_pygame_window
+
+
+def flip():
+    for b in button.all_buttons:
+        button.draw(b, misc.pg)
+
+
+def handle_event(event):
+    if len(button.all_buttons) > 0:
+        for b in button.all_buttons:
+            button.react(b, event)
+
+
+def handle_special_events():
+    if len(button.all_buttons) > 0:
+        for b in button.all_buttons:
+            button.react(b)
